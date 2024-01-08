92e8f5e3eb4cc3eb0035215088f6652839c4ba20
Mar-Issah <masy370@gmail.com>
2024-01-07 21:25:52 -0600

    Feat: implement client.py code

--- 0x03-Unittests_and_integration_tests/client.py
+++ 0x03-Unittests_and_integration_tests/client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""client.GithubOrgClient class"""
+"""A github org client
+"""
 from typing import (
     List,
     Dict,
@@ -13,40 +14,41 @@ from utils import (
 
 
 class GithubOrgClient:
-    """client.GithubOrgClient class"""
-    URL = "https://api.github.com/orgs/{org}"
+    """A Githib org client
+    """
+    ORG_URL = "https://api.github.com/orgs/{org}"
 
     def __init__(self, org_name: str) -> None:
         """Init method of GithubOrgClient"""
         self._org_name = org_name
 
     @memoize
-    def test_org(self) -> Dict:
-        """Org"""
-        return get_json(self.URL.format(org=self._org_name))
+    def org(self) -> Dict:
+        """Memoize org"""
+        return get_json(self.ORG_URL.format(org=self._org_name))
 
     @property
     def _public_repos_url(self) -> str:
         """Public repos URL"""
-        return self.test_org["repos_url"]
+        return self.org["repos_url"]
 
     @memoize
-    def repo_payload(self) -> Dict:
+    def repos_payload(self) -> Dict:
         """Memoize repos payload"""
         return get_json(self._public_repos_url)
 
     def public_repos(self, license: str = None) -> List[str]:
-        """Public repo"""
-        json_payload = self.repo_payload
-        repos = [
+        """Public repos"""
+        json_payload = self.repos_payload
+        public_repos = [
             repo["name"] for repo in json_payload
             if license is None or self.has_license(repo, license)
         ]
 
-        return repos
+        return public_repos
 
     @staticmethod
-    def test_has_license(repo: Dict[str, Dict], license_key: str) -> bool:
+    def has_license(repo: Dict[str, Dict], license_key: str) -> bool:
         """Static: has_license"""
         assert license_key is not None, "license_key cannot be None"
         try:
